84fdff893671b2be1a973e106878a933d292df20
yukikurage <55534323+yukikurage@users.noreply.github.com>
2022-01-15 10:08:14 +0900

    init commit

---
+++ esbuild.js
@@ -0,0 +1,10 @@
+require('esbuild').build({
+  entryPoints: ['entry.js'],
+  bundle: true,
+  outfile: 'public/index.js',
+  minify: false,
+  sourcemap: false,
+  target: ['chrome70', 'firefox57', 'safari11', 'edge16'],
+  watch: true,
+}).catch(() => process.exit(1))
+
